144cc6380ea91c99d427ed07a43f2af7b779d4e3
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-15 14:42:35 +0100

    hashset

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,42 +1,16 @@
 package sk.stuba.fei.uim.oop;
 
-import sk.stuba.fei.uim.oop.zvierata.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Map<String, String> hlavneMesta = new HashMap<>();
-        hlavneMesta.put("Slovensko", "Bratislava");
-        hlavneMesta.put("Cesko", "Praha");
-        hlavneMesta.put("Madarsko", "Budapest");
-        hlavneMesta.put("Polsko", "Varsava");
+        Set<String> auta = new HashSet<>();
+        auta.add("Volvo");
+        auta.add("BMW");
+        auta.add("Skoda");
+        auta.add("BMW");
 
-        System.out.println(hlavneMesta);
-        System.out.println(hlavneMesta.get("Slovensko"));
-        System.out.println("Mapa je velka : " + hlavneMesta.size());
-        System.out.println(hlavneMesta);
-
-        System.out.println("----");
-
-        System.out.println(hlavneMesta);
-        hlavneMesta.remove("Slovensko");
-        System.out.println(hlavneMesta);
-
-        System.out.println("----");
-
-//        System.out.println(hlavneMesta);
-//        hlavneMesta.clear();
-//        System.out.println(hlavneMesta);
-
-        System.out.println("----");
-
-        System.out.println("Mapa obsahuje slovensko :" + hlavneMesta.containsKey("Slovensko"));
-
-        for (String s : hlavneMesta.keySet()) {
-            System.out.println(hlavneMesta.get(s));
-        }
+        System.out.println(auta);
     }
 }
