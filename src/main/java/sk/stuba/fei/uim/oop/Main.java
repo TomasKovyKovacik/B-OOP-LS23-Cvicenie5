de1b271fbdca86993c7cfd1765d90a435d98e123
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2023-03-15 15:20:18 +0100

    streamy

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -5,7 +5,10 @@ import sk.stuba.fei.uim.oop.zvierata.Pes;
 import sk.stuba.fei.uim.oop.zvierata.Zviera;
 import sk.stuba.fei.uim.oop.zvierata.ZvieraComparator;
 
+import javax.crypto.Mac;
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,10 +18,24 @@ public class Main {
         zveri.add(new Macka("Murko"));
         zveri.add(new Macka("Garfield"));
 
-        System.out.println(zveri);
-        zveri.sort(new ZvieraComparator());
-        System.out.println(zveri);
+        List<String> mena = zveri.stream().map(Zviera::getMeno).collect(Collectors.toList());
+        mena.add("Tomas");
+        for (var meno: mena) {
+            System.out.println(meno);
+        }
+        System.out.println("-----");
 
+        List<Pes> psi = zveri.stream().filter(zviera -> zviera instanceof Pes).map(pes -> (Pes) pes).collect(Collectors.toList());
+        psi.forEach(Pes::zvukZvierata);
 
+        System.out.println("----");
+
+        List<Pes> psi2 = Collections.nCopies(5, new Pes("Dunco"));
+
+        System.out.println("----");
+
+        List<Macka> macky = Stream.generate(() -> new Macka("Murko")).limit(5).collect(Collectors.toList());
+
+        System.out.println("----");
     }
 }
